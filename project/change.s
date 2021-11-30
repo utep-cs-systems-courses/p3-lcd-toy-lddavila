0e3ff4471705fb9270bf3d3af1f4b2dee2c096fa
student <lddavila@miners.utep.edu>
2021-11-30 12:25:05 -0700

    change.s file created but not working

--- project/change.s
+++ project/change.s
@@ -1,16 +1,19 @@
 	.arch msp430
 	.p2align 1,0
-
-button:	 .word 0x00
 	
-jt:	 .word #case_0
-	 .word #case_1
+button:		 .word 0x00
+	
+jt:
+	.word #case_0
+	.word #case_1
+	
 	.global change
+	
 change:
 	mov #1, r12
-	cmp &state, r12
+	cmp &button, r12
 	jc end_case
-	move &button, r12
+	mov &button, r12
 case_0:
 	mov #-10, r12
 	jmp end_case
